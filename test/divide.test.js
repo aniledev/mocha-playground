29b60c0b681f382d5fd973924c0bf3441f466a4f
aniledev <elinamcgill@gmail.com>
2020-12-30 16:29:27 -0500

    feat: create function stub for test suite and test cases

--- test/divide.test.js
+++ test/divide.test.js
@@ -1,1 +1,13 @@
+// require the test file
 const divide = require("../index");
+// // require the chai assertion library
+// const chai = require("chai");
+// // require the expect interface of chai
+// const expect = chai.expect;
+const expect = require("chai").expect;
+
+// create test suite using the describe function
+describe("Divide function", () => {
+  it("should divide positive integers correctly", () => {});
+  it("should throw an error when divide by zero", () => {});
+});
