8029b1c813975385cb807493e0a2e9b8f0bf3ab0
aniledev <elinamcgill@gmail.com>
2020-12-31 15:46:55 -0500

    feat: refractor index.sum() to allow the function to add an array of number and numeric strings

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ const diff = (a, b) => {
 const sum = (arr) => {
   return new Promise((resolve, reject) => {
     const ans = arr.reduce((acc, curr) => {
-      return acc + curr;
+      const num = parseFloat(curr);
+      return acc + (isNaN(num) ? 0 : num);
     }, 0);
     resolve(ans);
   });
