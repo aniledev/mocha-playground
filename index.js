1c95711c289e4ddd8e4eaa20b8667d6738a5d2c7
aniledev <elinamcgill@gmail.com>
2020-12-30 19:29:52 -0500

    test: create stubs for test cases

--- index.js
+++ index.js
@@ -8,20 +8,16 @@ const divide = (a, b) => {
 
 module.exports == { divide, sort };
 
-const array = [8, 24, 16, 30, 10000, -8, -4, 50, 1001];
-
 function sort(list) {
   for (let i = 2; i < list.length; i++) {
-    let j = 1;
+    let j = i;
     while (j > 0 && list[j - 1] > list[j]) {
       let temp = list[j];
-      list[j] = list[j] - 1;
+      list[j] = list[j - 1];
       list[j - 1] = temp;
+
       j--;
     }
   }
-
   return list;
 }
-
-console.log(sort(array));
--- test/sort.test.js
+++ test/sort.test.js
@@ -1,11 +1,59 @@
 // require the test file
 // require the chai assertion library & require teh expect interface of chai
 const sort = require("../index");
-
 const expect = require("chai").expect;
 
-describe("Sort funcon", () => {
-  it("should", () => {
+// WHAT SHOULD THE FUNCTION BE ABLE TO DO
+// WHAT TYPE OF PARAMETERS
+// WHEN SHOULD IT THROW AN ERROR
+// WHAT FORMAT SHOULD THE RESULT BE IN
+
+describe("Sort function", () => {
+  it("should sort posiive integers in acsending order", () => {
+    // 1. define data inputs
+    //2. invoke the functio
+    // 3. assert the expected vs actual output
+  });
+  it("should sort positive integers in ascending order even when there are duplicates", () => {
+    // 1. define data inputs
+    //2. invoke the functio
+    // 3. assert the expected vs actual output
+  });
+  it("should sort negative integers in acsending order", () => {
+    // 1. define data inputs
+    //2. invoke the functio
+    // 3. assert the expected vs actual output
+  });
+  it("should sort a combination of positive and negative integers in acsending order", () => {
+    // 1. define data inputs
+    //2. invoke the functio
+    // 3. assert the expected vs actual output
+  });
+  // WHAT TO DO ABOUT FRACTIONS???
+  it("should sort floating point numbers in acsending order", () => {
+    // 1. define data inputs
+    //2. invoke the functio
+    // 3. assert the expected vs actual output
+  });
+  // throw an error if the paramter is not an array
+  it("should throw an error when the parameter is not an array", () => {
+    // 1. define data inputs
+    //2. invoke the functio
+    // 3. assert the expected vs actual output
+  });
+  // the returned value should in fact be an array
+  it("should return a value with the data type of array", () => {
+    // 1. define data inputs
+    //2. invoke the functio
+    // 3. assert the expected vs actual output
+  });
+  // the returned value should should have the same length as the parameter
+  it("should return a an array with the same length as the parameter", () => {
+    // 1. define data inputs
+    //2. invoke the functio
+    // 3. assert the expected vs actual output
+  });
+  it("should return an array with only data types of numbers", () => {
     // 1. define data inputs
     //2. invoke the functio
     // 3. assert the expected vs actual output
